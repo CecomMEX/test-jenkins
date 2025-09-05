db1b2326233583c9df67d9026c9a21da0b351fbb
Alexis Rodriguez <alexisrodriguez@Alexis-Rodriguez.local>
2025-09-05 17:55:35 -0600

    first

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `Hola bros NODE_ENV=${process.env.NODE_ENV ?? 'undefined'} como tan`;
+    return `NODE_ENV=${process.env.NODE_ENV ?? 'undefined'} ${process.env.DYNAMO_USER ?? 'ha'} best test`;
   }
 }
